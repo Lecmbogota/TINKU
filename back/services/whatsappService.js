c30f209e1fbafe9e76bff98f1b5f8a44e4722702
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 13:27:48 -0500

    Change header to headers in SendMessageWhatsapp function
    The code diff shows the modification made in the SendMessageWhatsapp function in the whatsappService.js file. The change is in the configuration object sent as a parameter to

--- back/services/whatsappService.js
+++ back/services/whatsappService.js
@@ -15,7 +15,7 @@ function SendMessageWhatsapp(textResponse, number) {
     path: "/v18.0/117778997940747/messages",
     method: "POST",
     body: data,
-    header: {
+    Headers: {
       "Content-Type": "application/json",
       Authorization:
         "Bearer EAAYKnYi8S9cBO4SKpG8ZCYgYUB5o56nAEM8QF1AbZCZBXq0bIhenadGG4J1kDv8615JmoZAX5ipCJu23lBfcRUPLBgjHGdZBgQkRTLhCktE0zWXFsZCdhY48dChOnxR7BSDq0bbyo9uge4GDV5xIMEpF6YDjV40Ki0FHx2YPABhrLrXJlAD7Ny7HGn2vhv5XYA",
