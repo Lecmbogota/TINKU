aa888e7ae52d1e4bedc97a0d32b8a5be26a6a287
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 17:00:42 -0500

    Change API base URL from localhost to production server in adminServices.js and authServices.js
    This commit changes the base URL of the API from 'http://localhost:3000' to 'webhook.caraballo

--- front/src/services/adminServices.js
+++ front/src/services/adminServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000'; // Reemplaza con la URL de tu servidor
+const BASE_URL = 'webhook.caraballo.pro'; // Reemplaza con la URL de tu servidor
 
 const api = axios.create({
   baseURL: BASE_URL,
