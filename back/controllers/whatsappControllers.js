940b825f7851bd4e4433bcc7765584d75add867d
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 12:13:20 -0500

    "Added console logging to receivedMessage function in whatsappControllers.js"
    The diff includes the addition of requiring the 'fs' module and creating a new console object that logs to a file named 'logs.txt'. The '

--- back/controllers/whatsappControllers.js
+++ back/controllers/whatsappControllers.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createReadStream("./logs.txt"));
+
 const verifyToken = (req, res) => {
   try {
     var accessToken = "960782506041989";
@@ -15,6 +18,17 @@ const verifyToken = (req, res) => {
 };
 
 const receivedMessage = (req, res) => {
+  try {
+    var entry = req.body["entry"][0];
+    var changes = req.body["changes"][0];
+    var value = changes["value"];
+    var messageObject = value["messages"];
+    myConsole.log(messageObject);
+    res.send("EVENT_RECEIVED");
+  } catch (error) {
+    res.send("EVENT_RECEIVED");
+  }
+
   res.send("hola Received");
 };
 
