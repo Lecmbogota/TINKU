44f7b29d2fa853eea075d1fc4992c8e4ce4e7a97
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 15:10:48 -0500

    "Updated WhatsApp service to include token replacement and response handling"
    This commit updates the WhatsApp service by replacing the hardcoded access token with a placeholder for the user's token and adding response handling for the request. The

--- back/services/whatsappService.js
+++ back/services/whatsappService.js
@@ -5,26 +5,31 @@ function SendMessageWhatsapp(data) {
     host: "graph.facebook.com",
     path: "/v18.0/117778997940747/messages",
     method: "POST",
-    body: data,
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer EAAYKnYi8S9cBO4SKpG8ZCYgYUB5o56nAEM8QF1AbZCZBXq0bIhenadGG4J1kDv8615JmoZAX5ipCJu23lBfcRUPLBgjHGdZBgQkRTLhCktE0zWXFsZCdhY48dChOnxR7BSDq0bbyo9uge4GDV5xIMEpF6YDjV40Ki0FHx2YPABhrLrXJlAD7Ny7HGn2vhv5XYA",
+      "Authorization": "Bearer EAAYKnYi8S9cBO4SKpG8ZCYgYUB5o56nAEM8QF1AbZCZBXq0bIhenadGG4J1kDv8615JmoZAX5ipCJu23lBfcRUPLBgjHGdZBgQkRTLhCktE0zWXFsZCdhY48dChOnxR7BSDq0bbyo9uge4GDV5xIMEpF6YDjV40Ki0FHx2YPABhrLrXJlAD7Ny7HGn2vhv5XYA", // Reemplaza esto con tu token de acceso
     },
   };
 
   const req = https.request(options, (res) => {
+    let responseData = '';
+
     res.on("data", (d) => {
-      process.stdout.write(d);
+      responseData += d;
+    });
+
+    res.on("end", () => {
+      console.log("Respuesta de la solicitud:", responseData);
+      // Puedes realizar más acciones con la respuesta aquí, según tus necesidades
     });
   });
 
   req.on("error", (error) => {
-    console.error(error);
+    console.error("Error al enviar la solicitud:", error);
   });
 
-  req.write(data);
-  req.end(); // Llama a la función end() para finalizar la solicitud
+  req.write(JSON.stringify(data)); // Escribe el cuerpo del mensaje en la solicitud
+  req.end(); // Finaliza la solicitud
 }
 
 module.exports = {
