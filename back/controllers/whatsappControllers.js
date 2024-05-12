8b84f9e2456af71f67fe731f24ec91e0cd31ece4
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 19:05:32 -0500

    "Replace res.json({ contacts: contacts }) with res.json(contacts) in getReceivedMessages"

--- back/controllers/whatsappControllers.js
+++ back/controllers/whatsappControllers.js
@@ -69,7 +69,7 @@ const receivedMessage = (req, res) => {
 
 const getReceivedMessages = (req, res) => {
   try {
-    res.json({ contacts: contacts });
+    res.json( contacts );
   } catch (error) {
     console.error("Error al obtener los mensajes recibidos:", error);
     res.status(500).send("Error en el servidor");
