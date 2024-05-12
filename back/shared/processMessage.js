a8ac303fa210d282b0fd3d6382a91df0fc6118b9
Luis Caraballo <lecmbogota@gmail.com>
2024-05-11 20:30:16 -0500

    "Refactor Process function to reduce code duplication and improve readability"
    The Process function has been refactored to use an array of models and a forEach loop to send messages, reducing code duplication and improving readability. The

--- back/shared/processMessage.js
+++ back/shared/processMessage.js
@@ -40,43 +40,6 @@ function Process(textUser, number) {
 };
 
 
-function Process(textUser, number) {
-  textUser = textUser.toLowerCase(); // Corregir función toLowerCase
-  var models = [];
-
-  if (textUser.includes("hola")) {
-    // Saludar
-    var model = whatsappModel.MessageText("Hola, ¡un gusto saludarte!", number);
-    models.push(model);
-  }
-
-  else if (textUser.includes("adios") || textUser.includes("adiós") || textUser.includes("chao")) {
-    // Despedida
-    var model = whatsappModel.MessageText("Gracias por contactarnos ❤", number);
-    models.push(model);
-  }
-  else if (textUser.includes("que tal")) {
-    // Despedida
-    var model = whatsappModel.MessageText("todo bien y tu?", number);
-    models.push(model);
-  }
-  else if (textUser.includes("amor")) {
-    // Despedida
-    var model = whatsappModel.MessageText("adriana, santiago y sebastian", number);
-    models.push(model);
-  }
-
-  else{
-    // No entiende el mensaje del usuario
-    var model = whatsappModel.MessageText("No entiendo lo que dices", number);
-    models.push(model);
-  }
-
-  // Enviar todos los modelos
-  models.forEach(model => {
-    whatsappService.SendMessageWhatsapp(model);
-  });
-};
 function ProcessAgent(textResponse, number){
   var agent = whatsappModel.MessageText(textResponse, number);
   whatsappService.SendMessageWhatsapp(agent);
